cc08093ef015fff031a7044f021d57b2be726dbb
WhanderingGhosts <tiagoramon.sousa12@gmail.com>
2024-08-24 22:11:38 -0300

    Polimento dos inputs

--- src/assets/components/InputNome.jsx
+++ src/assets/components/InputNome.jsx
@@ -10,7 +10,7 @@ function InputNome() {
                 placeholder="Digite seu nome..."
                 className="h-6 px-4 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none focus:invalid:ring-pink-50" />
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none" />
         </div>
     )
 }
